2fc5e8b7497035472e0d74f602d21015011f233d
Tiago Gomes <tiagogomes772@gmail.com>
2023-01-14 16:36:22 +0000

    Added home page with photo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,76 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import banner from '$lib/images/banner.jpg';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+  <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Josefin+Sans:wght@100&display=swap" rel="stylesheet">
+</svelte:head>
+
+<section class="section">
+  <img class="image" alt="Banner" src={banner} />
+  <div class="text">
+    <h2 class="support-text"> Mais novidades em breve</h2>
+    
+    <div class="main-text">
+      <h1 class="title">
+        Diana e Tiago
+        <br />
+      </h1>
+      <h3 class="subtitle">
+        27 Maio 2023
+        <br />
+      </h3>
+    </div>
+  </div>
+
+</section>
+
+<style>
+
+  .section {
+    position: relative;
+  }
+
+  .image {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
+
+  .main-text {
+    font-family: 'Great Vibes', cursive;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    color: white;
+    width: 100%;
+    margin: 16px;
+    margin-bottom: 50px;
+  }
+
+  .title {
+    font-size: 60px;
+    margin: 0;
+    font-weight: 400;
+
+  }
+
+  .subtitle {
+    font-family: 'Josefin Sans', sans-serif;
+    margin: 0;
+    font-size: 20px;
+    font-weight: 700;
+  }
+  
+  .support-text {
+    font-family: 'Josefin Sans', sans-serif;
+    position: absolute;
+    bottom: 0;
+    right: 5px;
+    font-size: 20px;
+    color: white;
+  }
+
+</style>
